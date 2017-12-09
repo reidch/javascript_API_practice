bcbf08ee9749a233ac44fb67c2e93be7c67d0318
Caroline Reid <carolinehreid@gmail.com>
2017-12-09 21:33:26 +0000

    makes columnChart 3D

--- public/columnChart.js
+++ public/columnChart.js
@@ -5,16 +5,41 @@ var ColumnChart = function(){
 	var chart = new Highcharts.chart({
 		chart: {
 			type: 'column',
+			options3d: {
+				enabled: true,
+				alpha: 10,
+				beta: 25,
+				depth: 70
+			},
 			renderTo: container
 		},
 		title: {
-			text: "Rotten Tomatoes rating (out of 100)"
+			text: "Rotten Tomatoes rating"
 		},
-		series: [
-			{ name: "Films", data: [95, 97, 93] }
-		],
+		plotOptions: {
+			column: {
+				depth: 25
+			}
+		},
+		series: [{
+			name: "Films",
+			data: [95, 97, 93]
+		}],
 		xAxis: {
-			categories: ['Castle in the Sky', 'Grave of the Fireflies', 'My Neighbor Totoro']
+			categories: ['Castle in the Sky', 'Grave of the Fireflies', 'My Neighbor Totoro'],
+			labels: {
+		skew3d: true,
+		style: {
+				fontSize: '16px'
+		}
+}
+		},
+		yAxis: {
+			min: 0,
+			max: 100,
+			title: {
+				text: 'Rating out of 100'
+			}
 		},
 	});
 };
