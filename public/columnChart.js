6f52364c44aed344a30478fa6eeb65b19d4c9f33
Caroline Reid <carolinehreid@gmail.com>
2017-12-09 22:23:38 +0000

    adds data for columnChart

--- public/columnChart.js
+++ public/columnChart.js
@@ -22,23 +22,24 @@ var ColumnChart = function(){
 			}
 		},
 		series: [{
-			name: "Films",
-			data: [95, 97, 93]
+			name: "Rating",
+			data: [95, 97, 93, 96, 100, 94, 78, 91, 92, 75, 97, 89, 87, 41, 92, 95, 83, 89, 100, 92],
+			color: "tomato"
 		}],
 		xAxis: {
-			categories: ['Castle in the Sky', 'Grave of the Fireflies', 'My Neighbor Totoro'],
+			categories: ["Castle in the Sky", "Grave of the Fireflies", "My Neighbor Totoro", "Kiki's Delivery Service", "Only Yesterday", "Porco Rosso", "Pom Poko", "Whisper of the Heart", "Princess Mononoke", "My Neighbors the Yamadas", "Spirited Away", "The Cat Returns", "Howl's Moving Castle", "Tales from Earthsea", "Ponyo", "Arrietty", "From Up on Poppy Hill", "The Wind Rises", "The Tale of the Princess Kaguya", "When Marnie Was There" ],
 			labels: {
 		skew3d: true,
 		style: {
 				fontSize: '16px'
-		}
-}
+				}
+			}
 		},
 		yAxis: {
 			min: 0,
 			max: 100,
 			title: {
-				text: 'Rating out of 100'
+				text: " "
 			}
 		},
 	});
